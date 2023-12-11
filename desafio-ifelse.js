3e2dc9071b69dc07c3172b1fcd790e5698928f4c
Ana Dantas <71779858+AnaDantas00@users.noreply.github.com>
2023-12-10 21:11:35 -0300

    Create desafio-ifelse.js

---
+++ desafio-ifelse.js
@@ -0,0 +1,36 @@
+let nomeDoHeroi = "Gargula o Sanguinario"
+let xp = 20000
+
+if(xp <= 1000){
+    console.log("O herói " + nomeDoHeroi + "está no nível Ferro")
+}
+
+else if (xp >1000 && xp <=2000){
+    console.log("O herói " + nomeDoHeroi + " está no nível Bronze")
+}
+
+else if (xp >2000 && xp <=5000){
+    console.log("O herói " + nomeDoHeroi + " está no nível Prata")
+}
+
+else if (xp >6000 && xp <=7000){
+    console.log("O herói " + nomeDoHeroi + " está no nível Ouro")
+}
+
+
+else if (xp >7000 && xp <=8000){
+    console.log("O herói " + nomeDoHeroi + " está no nível Platina")
+}
+
+else if (xp >8000 && xp <=9000){
+    console.log("O herói " + nomeDoHeroi + " está no nível Ascendente")
+}
+
+
+else if(xp >9000 && xp <= 1000){
+    console.log("O herói "+ nomeDoHeroi + "está no nível Imortal")
+}
+
+else if(xp >1000){
+    console.log("O herói " + nomeDoHeroi + "está no nível IMPLACAVEL")
+}
