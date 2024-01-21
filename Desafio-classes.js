90f6333dcecf6ea5ea2c7a856f6b16d57c0b3dfa
Ana Dantas <71779858+AnaDantas00@users.noreply.github.com>
2024-01-21 19:43:46 +0000

    Pending changes exported from your codespace

--- Desafio-classes.js
+++ Desafio-classes.js
@@ -1,1 +1,17 @@
-JS
+const classeHeroi = ["Guerreiro", "Mago", "Monge", "Ninja"]
+const ataqueClasse = ["Espada", "Magia", "Artes marciais", "Shuriken"]
+let nameHeroi = "Hercules"
+let idadeHeroi = "28"
+let skill = "Guerreiro"
+
+
+
+
+if (skill = "Guerreiro"){
+    console.log("O herói " + classeHeroi[0] + " atacou usando " + ataqueClasse[0])
+} else if (skill = "Mago"){
+    console.log("O herói " + classeHeroi[1] + " atacou usando " + ataqueClasse[1])
+} else if (skill = "Monge"){
+    console.log("O herói " + classeHeroi[2] + " atacou usando " + ataqueClasse[2])
+} else if (skill = "Ninja")
+console.log("O herói " + classeHeroi[3] + " atacou usando " + ataqueClasse[3])
